8edfc724781c400bd627d5fdd529fe58867ade73
Bruno Konrad <bruno.skon@gmail.com>
2016-04-14 23:30:39 -0300

    Change the API to use a jQuery plugin to start

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-function convert(str) {
-  return str.replace(/-([a-z])/g, function (g) {
-    return g[1].toUpperCase();
-  });
-}
-
-function initialize($) {
-  $('[data-component]').each(function() {
-    var component, options;
-    var $this = $(this);
-    var plugin = $this.data('component');
-
-    if (!plugin) {
-      return;
-    }
-
-    component = convert(plugin);
-    options = $this.data();
-
-    if (component in $this) {
-      $this[component](options);
-    }
-  });
-}
-
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     define(['jquery'], factory);
@@ -43,5 +18,51 @@ function initialize($) {
     factory(jQuery);
   }
 }(function ($) {
+  function convert(str) {
+    return str.replace(/-([a-z])/g, function (g) {
+      return g[1].toUpperCase();
+    });
+  }
+
+  function startComponents($components) {
+    $components.each(function() {
+      var component, options;
+      var $this = $(this);
+      var plugin = $this.data('component');
+
+      if (!plugin) {
+        return;
+      }
+
+      component = convert(plugin);
+      options = $this.data();
+
+      if (component in $this) {
+        $this.attr('data-component-started', true);
+        $this[component](options);
+      }
+    });
+  }
+
+  function initialize($) {
+    $.fn.startComponents = function(options) {
+      options = options || {};
+
+      var $root = $(this);
+      var restart = options.restart || false;
+
+      if (restart) {
+        startComponents($root.find('[data-component]'));
+        return this;
+      }
+
+      startComponents($root
+        .find('[data-component]')
+        .not('[data-component-started]')
+      );
+      return this;
+    }
+  }
+  
   initialize($);
 }));
--- specs/jquery_components.spec.js
+++ specs/jquery_components.spec.js
@@ -9,22 +9,13 @@ describe('initialize jQuery plugins as components', function() {
     $('[data-component]').remove();
   });
 
-  describe('convert function', function() {
-    it('converts from hyphen separated to camelCase', function() {
-      expect(convert('foo-bar')).toBe('fooBar');
-    });
-
-    it("doesn't change the input value", function() {
-      expect(convert('fooBar')).toBe('fooBar');
-    });
-  });
-
-  describe('initialize function', function() {
+  describe('initialize components', function() {
     it('initializes the element with hyphen named component', function() {
       var fixture = '<div data-component="some-plugin"></div>';
-      $('body').append(fixture);
 
-      initialize(jQuery);
+      $('body')
+        .append(fixture)
+        .startComponents();
 
       expect($.fn.somePlugin).toHaveBeenCalled();
     });
@@ -32,29 +23,66 @@ describe('initialize jQuery plugins as components', function() {
     it('initializes both plugins', function() {
       var fixture = '<div data-component="somePlugin"></div>' +
         '<div data-component="plugin"></div>';
-      $('body').append(fixture);
 
-      initialize(jQuery);
+      $('body')
+        .append(fixture)
+        .startComponents();
 
       expect($.fn.plugin).toHaveBeenCalled();
       expect($.fn.somePlugin).toHaveBeenCalled();
     });
-  });
 
-  describe('initializes getting data attributes options', function() {
-    it('fo', function() {
+    it('initializes getting data attributes options', function() {
       var fixture = '<div data-component="carousel" ' +
         'data-itens="3" data-infinite="true" data-name="selly">' +
         '</div>';
-      $('body').append(fixture);
 
-      initialize(jQuery);
+      $('body')
+        .append(fixture)
+        .startComponents();
 
       expect($.fn.carousel).toHaveBeenCalledWith(jasmine.objectContaining({
         itens: 3,
         infinite: true,
         name: 'selly'
       }));
+    });
+
+    it('checks if the initialized component has started flag', function() {
+      var fixture = '<div data-component="some-plugin"></div>';
+
+      $('body')
+        .append(fixture)
+        .startComponents();
+
+      var wasStarted = $('[data-component]')
+        .attr('data-component-started');
+
+      expect(wasStarted).toBeTruthy();
+    });
+  });
+
+  describe('when a component is already started', function() {
+    it("don't try to start again", function() {
+      var fixture = '<div data-component="carousel" ' +
+        'data-component-started="true"></div>';
+
+      $('body')
+        .append(fixture)
+        .startComponents();
+
+      expect($.fn.carousel).not.toHaveBeenCalled();
+    });
+
+    it('mark a flag to start anyway', function() {
+      var fixture = '<div data-component="carousel" ' +
+        'data-component-started="true"></div>';
+
+      $('body')
+        .append(fixture)
+        .startComponents({restart: true});
+
+      expect($.fn.carousel).toHaveBeenCalled();
     });
   });
 });
